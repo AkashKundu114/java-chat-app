f8e616f63c09f068c45b54ce1789dc0b90d06517
AkashKundu114 <68686532+AkashKundu114@users.noreply.github.com>
2025-12-08 20:03:42 +0530

    MKB AAG!

--- 3_client_web/src/hooks/useChatBridge.js
+++ 3_client_web/src/hooks/useChatBridge.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 
-const WS_URL = 'ws://localhost:8080'; 
+const WS_URL = 'wss://johnnywalker.loca.lt'; 
 
 export const useChatBridge = () => {
   const [socket, setSocket] = useState(null);
